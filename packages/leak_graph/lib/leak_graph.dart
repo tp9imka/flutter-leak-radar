ee0f59322ee93edc519eec8ab8fd67509c23d10d
Ivan Antsimonau <tp9imka@gmail.com>
2026-06-24 19:21:21 +0400

    feat(leak_graph): denylist root classifier (timer/stream/closure/finalizer/static)

--- packages/leak_graph/lib/leak_graph.dart
+++ packages/leak_graph/lib/leak_graph.dart
@@ -13,3 +13,4 @@ export 'src/model/graph_leak_cluster.dart';
 export 'src/model/graph_analysis_result.dart';
 export 'src/graph/heap_graph_view.dart';
 export 'src/analysis/shortest_retaining_paths.dart';
+export 'src/analysis/root_classifier.dart';
