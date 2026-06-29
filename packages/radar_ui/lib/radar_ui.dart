45fe3f214d1b7133929b94cf877ff89c21d14d51
tp9imka <tp9imka@gmail.com>
2026-06-29 15:40:56 +0400

    feat(radar_ui): add RadarSearchField widget

--- packages/radar_ui/lib/radar_ui.dart
+++ packages/radar_ui/lib/radar_ui.dart
@@ -8,3 +8,4 @@ export 'src/theme/radar_theme.dart';
 export 'src/widgets/radar_tag.dart';
 export 'src/widgets/radar_sparkline.dart';
 export 'src/widgets/radar_metric_tile.dart';
+export 'src/widgets/radar_search_field.dart';
